6472806e24233824aace3721a104adf6b1be7e9a
kaviiarasu <kaviiarasu25@gmail.com>
2021-12-31 16:34:01 +0530

    password functionality

--- src/org/GitKavi1/BaseGit.java
+++ src/org/GitKavi1/BaseGit.java
@@ -1,5 +1,12 @@
 package org.GitKavi1;
 
 public class BaseGit {
+	public static void password() {
+		System.out.println("password");
+	}
+	public static void main(String[] args) {
+		password();
+	}
+	
 
 }
